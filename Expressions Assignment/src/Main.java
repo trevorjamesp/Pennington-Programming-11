8d62ff4c940659a62f3aa6e2c4487b53c71d4229
trevorjamesp <75449543+trevorjamesp@users.noreply.github.com>
2020-12-06 17:45:02 -0800

    Expressions Hand In

---
+++ Expressions Assignment/src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String args[]){
+        /*For the following expressions write out using code step by step how the
+        compiler will evaluate it
+        Example:
+        System.out.println(2 * 5 + 3);
+        System.out.println(10 + 3);
+        System.out.println(13);
+        Only complete one computation per line of code
+        The output for every line is always equal to the same number
+        In example above the out put should be
+        13
+        13
+        13
+        */
+        //Expression 1
+        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println((5) + 5 - (-4)/3);
+        System.out.println(5 + 5 - (-1));
+        System.out.println(10 + 1);
+        System.out.println(11);
+        // Answer = 11
+
+        //Expression 2
+        System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(3 + (18 - 1));
+        System.out.println(3 + 17);
+        System.out.println(20);
+        // Answer = 20
+
+        //Expression 3
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println(((3) + (81)/5));
+        System.out.println(3 + 16);
+        System.out.println(19);
+        // Answer = 19
+
+        //Expression 4
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.Println((1) - 8 + (8) / 8 + (9)/3);
+        System.out.println(-7 + 1 + 3);
+        System.out.println(-3);
+        // Answer -3
+
+        //Expression 5
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(math.sqrt(25), 3));
+        System.out.println((10 * 0.8509 + 2) - math.pow(5, 3));
+        System.out.println(10.5090 - 125);
+        System.out.println(-114.49096);
+        // Answer -114.49096
+    }
+}
